ab2b3164495ce3b34fc8c9709347efd5481810e5
Salman <salmanmdsultan@gmail.com>
2018-03-26 01:43:36 -0700

    Admin php file init

--- SMS-ERP-V1/routes/web.php
+++ SMS-ERP-V1/routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/insert_result', function () {
+    return view('insert_result');
+});
+Route::get('/search_result', function () {
+    return view('search_result');
+});
+Route::get('/show_info', function () {
+    return view('show_info');
+});
+Route::get('/show_result', function () {
+    return view('show_result');
+});
+Route::get('/stu_info_new', function () {
+    return view('stu_info_new');
+});
